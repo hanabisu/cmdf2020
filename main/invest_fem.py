095f8df4f4f2ea0f138cd6fca5510ac58ee60b72
hanabisu <gabouryn@students.cs.ubc.ca>
2020-03-07 19:23:44 -0800

    add saving input text to variable and image to background

--- main/invest_fem.py
+++ main/invest_fem.py
@@ -1,7 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.floatlayout import FloatLayout
 from os import path
+from kivy.config import Config
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '600')
 # Create both screens. Please note the root.manager.current: this is how
 # you can control the ScreenManager from kv. Each screen has by default a
 # property manager that gives you the instance of the ScreenManager used.
@@ -29,9 +33,15 @@ sm.add_widget(EnterItemScreen(name='enter_item'))
 sm.add_widget(FirstStepScreen(name='first_step'))
 
 class InvestFemme(App):
-
+    balance = 100
     def build(self):
+        balance = 100
         return sm
+
+    def getItemName(self):
+        itemName = self.root.get_screen('enter_item').ids.txt_input.text
+        print(itemName)
+        sm.switch_to(FirstStepScreen(name='first_step'))
 
 if __name__ == '__main__':
     InvestFemme().run()
